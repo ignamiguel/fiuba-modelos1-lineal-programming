79191a9c407da42fc34e636dbeac9b681a5701fa
Ignacio Iglesias <ignacio.iglesias@mulesoft.com>
2022-04-09 11:04:33 -0300

    Added e2.1

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # fiuba-modelos1-lineal-programming
 FIUBA Modelos 1 Lineal Programming
+
+Command
+```bash
+glpsol -m mod1.lp -o mod1.sol
+```
+
+Documentation
+https://www.gnu.org/software/glpk/
---
+++ e2.1.sol
@@ -0,0 +1,44 @@
+Problem:    e2
+Rows:       7
+Columns:    5
+Non-zeros:  15
+Status:     OPTIMAL
+Objective:  z = 550 (MAXimum)
+
+   No.   Row name   St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 z            B            550                             
+     2 procEq1      NU            20                          20           2.5 
+     3 procEq2      B             30                          36 
+     4 procEq3      NU            80                          80           2.5 
+     5 procEq4      NU            80                          80          3.75 
+     6 procEq5      NS             0            -0             =         < eps
+     7 procEq6      B        16.6667            10               
+
+   No. Column name  St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 A            NL             0             0                        -6.5 
+     2 B            B        16.6667             0               
+     3 C            B        16.6667             0               
+     4 B_1          B        13.3333             0               
+     5 B_2          B        3.33333             0               
+
+Karush-Kuhn-Tucker optimality conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.DE: max.abs.err = 0.00e+00 on column 0
+        max.rel.err = 0.00e+00 on column 0
+        High quality
+
+KKT.DB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
